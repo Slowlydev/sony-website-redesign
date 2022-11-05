5e86cb41a02f7f1aa2ec94f8b37aab7e0b027884
Slowlydev <slowly.developers@gmail.com>
2022-11-05 22:54:03 +0100

    refactor: mobile improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+"use client";
+
 import classNames from "classnames";
 import Image from "next/image";
+import Link from "next/link";
 
 import styles from "./HomePage.module.scss";
 
-import cam from "../public/assets/cameras/ILCE-7RM5.jpeg";
+import { useScreenSize } from "../lib/context/ScreenSizeProvider";
+
+import Footer from "../components/Footer/Footer";
+
+import cam from "../public/products/ILCE-7RM5.png";
 
 import bike from "../public/assets/images/bike.jpg";
 import clouds from "../public/assets/images/clouds.jpg";
@@ -14,21 +21,26 @@ import skyscrapers from "../public/assets/images/skyscrapers.jpg";
 
 const images = [
 	{ src: bike, alt: "Bike", portrait: false, pos: { x: -300, y: 200, rotation: -10 } },
+	{ src: rocks, alt: "Rocks", portrait: true, pos: { x: 100, y: 250, rotation: 15 } },
 	{ src: clouds, alt: "Clouds", portrait: true, pos: { x: -100, y: -250, rotation: 10 } },
 	{ src: landscape, alt: "Landscape", portrait: false, pos: { x: 300, y: -200, rotation: 3 } },
-	{ src: rocks, alt: "Rocks", portrait: true, pos: { x: 100, y: 250, rotation: 15 } },
+
 	{ src: matterhorn, alt: "Matterhorn", portrait: true, pos: { x: 450, y: 200, rotation: -10 } },
 	{ src: skyscrapers, alt: "Skyscrapers", portrait: true, pos: { x: -450, y: -200, rotation: -10 } },
 ];
 
 export default function Page() {
+	const { screenWidth } = useScreenSize();
+
+	const finalImages = images.slice(0, screenWidth > 1000 ? 6 : 4);
+
 	return (
 		<>
 			<section className={classNames([styles.page, styles.hero])}>
 				<h1>Create Stunning Pictures</h1>
 
 				<div className={styles.imageContainer}>
-					{images.map((image) => (
+					{finalImages.map((image) => (
 						<Image
 							key={`image.${image.alt}`}
 							style={{ transform: `translate(${image.pos.x}px, ${image.pos.y}px) rotate(${image.pos.rotation}deg)` }}
@@ -43,15 +55,21 @@ export default function Page() {
 
 			<section className={classNames([styles.page, styles.second])}>
 				<div className={styles.content}>
-					<h1>The new Sony α7R V </h1>
-					<Image src={cam} alt="" />
+					<h1>Die neue Sony α7R V </h1>
+					<Image className={styles.secondImage} src={cam} alt="Sony α7R V" />
 					<p>
 						Mit der α7R V können Sie Ihre Visionen noch besser umsetzen - mit dedizierter KI-Verarbeitung zum Erkennen und Verfolgen Ihrer Motive.
 						Fotografen profitieren von einem völlig neuen Maß an Bildqualität und Auflösung, Videoaufnahmen mit 8K, stark verbesserter
 						Bildstabilisierung mit 8 Stufen sowie erweiterter Konnektivität und fortschrittlicheren Workflows.
 					</p>
+
+					<Link href="/products/ilce-7rm5">
+						<button>Details</button>
+					</Link>
 				</div>
 			</section>
+
+			<Footer />
 		</>
 	);
 }
--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { motion } from "framer-motion";
+import { useEffect } from "react";
 import Image from "next/image";
-import Link from "next/link";
+import classNames from "classnames";
+
+import { useScreenSize } from "../../lib/context/ScreenSizeProvider";
 
 import { ProductType } from "../../types/Product";
 
@@ -14,18 +18,29 @@ type ProductCardProps = {
 };
 
 export default function ProductCard({ product }: ProductCardProps) {
+	const { mobile } = useScreenSize();
+	const router = useRouter();
+
+	useEffect(() => {
+		router.prefetch(`/products/${product.slug}`);
+	}, []);
+
 	return (
-		<Link href={`/products/${product.slug}`}>
-			<motion.div className={styles.productCard} initial={{ scale: 1 }} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-				<Image alt={product.image.alt} src={`/products/${product.image.src}`} width={660} height={660} className={styles.image} />
+		<motion.div
+			onClick={() => router.push(`/products/${product.slug}`)}
+			className={classNames(styles.productCard, { [styles.mobile]: mobile })}
+			initial={{ scale: 1 }}
+			whileHover={{ scale: 1.1 }}
+			whileTap={{ scale: 0.9 }}
+		>
+			<Image alt={product.image.alt} src={`/products/${product.image.src}`} width={660} height={660} className={styles.image} />
 
-				<div className={styles.textContainer}>
-					<p>{product.name}</p>
-					<p>{product.marketingProductName}</p>
+			<div className={styles.textContainer}>
+				<p>{product.name}</p>
+				<p>{product.marketingProductName}</p>
 
-					<p className={layoutStyles.price}>{product.price} CHF</p>
-				</div>
-			</motion.div>
-		</Link>
+				<p className={layoutStyles.price}>{product.price} CHF</p>
+			</div>
+		</motion.div>
 	);
 }
